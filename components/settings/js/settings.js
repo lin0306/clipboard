46744ed0ae4418ccd114dd16d8d3c35487576b11
linweijia <2138388112@qq.com>
2025-03-05 18:14:48 +0800

    设置页面样式修改

--- components/settings/js/settings.js
+++ components/settings/js/settings.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // 初始化设置菜单交互
     const menuItems = document.querySelectorAll('.settings-menu li');
     const containers = document.querySelectorAll('.settings-container > div');
+
+    // 初始化select元素交互
+    document.querySelectorAll('select').forEach(select => {
+        select.addEventListener('focus', () => {
+            select.parentElement.querySelector('.arrow-icon').classList.add('rotate');
+        });
+        select.addEventListener('blur', () => {
+            select.parentElement.querySelector('.arrow-icon').classList.remove('rotate');
+        });
+        select.addEventListener('change', function () {
+            this.blur();
+            this.parentNode.querySelector('.arrow-icon').classList.remove('rotate');
+        });
+    });
+    document.querySelectorAll('option').forEach(option => {
+        option.addEventListener('click', () => {
+            document.querySelectorAll('.arrow-icon').classList.remove('rotate');
+        });
+    });
 
     // 默认激活第一个标签页
     menuItems[0].classList.add('active');
